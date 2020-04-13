3368e704c220f5d9ef41d9a15fadd045f750be69
hz <869932173@qq.com>
2020-04-13 11:24:38 +0800

    springboot-beanNameGenerator @Service等未命名的组件生成bean name时使用自定义beanNameGenerator

--- springboot-beanNameGenerator/src/main/java/com/springboot/beanname/BeannameApplication.java
+++ springboot-beanNameGenerator/src/main/java/com/springboot/beanname/BeannameApplication.java
@@ -2,11 +2,15 @@ package com.springboot.beanname;
 
 import com.springboot.beanname.filter.MyTypeFilter;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication
+//@SpringBootApplication （相当于如下三个注解）
+@SpringBootConfiguration
+@EnableAutoConfiguration
 @ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.CUSTOM, classes = MyTypeFilter.class)})
 public class BeannameApplication {
     public static void main(String[] args) {
