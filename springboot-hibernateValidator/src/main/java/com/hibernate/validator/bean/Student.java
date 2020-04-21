06495a0e351f787f4ba60f3980aa77af7a92ff7c
hz <869932173@qq.com>
2020-04-21 17:01:25 +0800

    springboot-hibernate validator

---
+++ springboot-hibernateValidator/src/main/java/com/hibernate/validator/bean/Student.java
@@ -0,0 +1,21 @@
+package com.hibernate.validator.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Size;
+
+/**
+ * @author hz
+ * @create 2020-04-10
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String name;
+    @Size(min = 1, max = 2)
+    private Integer age;
+
+}
